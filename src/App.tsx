442c9b1cc0bb5c15fc6182cc962e788a1f8de80f
Xrani <vladislavfrod@gmail.com>
2024-07-04 18:54:49 +0300

    Complected lesson3

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-import React, {FC} from 'react';
+import React, {FC, useState} from 'react';
 import './App.css';
 import {useToggle} from "./hooks/useToggle";
+import {usePrevious} from "./hooks/usePrevious";
+
 
 const App: FC = () => {
     const {value, handleToggle} = useToggle(false);
+    const [count, setCount] = useState(0);
+    const [currentCount, previousCount] = usePrevious(count);
 
     return (
         <div>
             <p>{value? 'True': 'False'}</p>
             <button onClick={handleToggle}>Click</button>
+            <p>Current count: {currentCount}</p>
+            <p>Previous count: {previousCount}</p>
+            <button onClick={() => setCount(count + 1)}>Increment</button>
+            <button onClick={() => setCount(count - 1)}>Decrement</button>
         </div>
     );
 };
--- src/hooks/useToggle.tsx
+++ src/hooks/useToggle.tsx
@@ -2,6 +2,6 @@ import {useState} from "react"
 
 export const useToggle = (initValue: boolean) => {
     const [value, setValue] = useState<boolean>(initValue)
-    const handleToggle = () => setValue((currentValue: boolean) => !currentValue);
+    const handleToggle = () => setValue((boolValue: boolean) => !boolValue);
     return {value, handleToggle};
 }
